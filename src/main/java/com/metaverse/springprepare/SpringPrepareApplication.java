dbf39f01d18ae3a35e6f6ca4e9f8db0dd412ceb4
citeFred <citefred@gmail.com>
2025-07-02 11:20:08 +0900

    chore: First init

---
+++ src/main/java/com/metaverse/springprepare/SpringPrepareApplication.java
@@ -0,0 +1,12 @@
+package com.metaverse.springprepare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrepareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrepareApplication.class, args);
+    }
+}
